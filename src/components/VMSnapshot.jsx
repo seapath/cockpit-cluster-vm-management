39d1afcef06cfb8ed39bc437ea39839c29b480c2
Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>
2024-06-05 13:36:05 +0200

    VMSnapshot: add React component
    This component opens a small window on the cockpit plugin page to write
    the name of the snapshot to be created.
    
    Signed-off-by: Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>

---
+++ src/components/VMSnapshot.jsx
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2024 Savoir-faire Linux Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import { Modal, ModalVariant, Button, Form, FormGroup, TextInput } from '@patternfly/react-core';
+
+class VMSnapshot extends React.Component {
+  state = {
+    snapshotName: '',
+  };
+
+  handleSnapshotNameChange = (e) => {
+    this.setState({ snapshotName: e.target.value });
+  };
+
+  handleConfirm = () => {
+    const { snapshotName } = this.state;
+    this.props.onConfirm(snapshotName);
+  };
+
+  handleCancel = () => {
+    this.setState({ snapshotName: '' });
+    this.props.onCancel();
+  };
+
+  render() {
+    const { isOpen } = this.props;
+    const { snapshotName } = this.state;
+
+    return (
+      <Modal
+        title="Create Snapshot"
+        variant={ModalVariant.small}
+        isOpen={isOpen}
+        onClose={this.handleCancel}
+        actions={[
+          <Button key="confirm" variant="primary" onClick={this.handleConfirm}>
+            Confirm
+          </Button>,
+          <Button key="cancel" variant="link" onClick={this.handleCancel}>
+            Cancel
+          </Button>
+        ]}
+      >
+        <Form>
+          <FormGroup label="Snapshot Name" isRequired fieldId="snapshot-name">
+            <TextInput
+              isRequired
+              type="text"
+              id="snapshot-name"
+              name="snapshotName"
+              value={snapshotName}
+              onChange={this.handleSnapshotNameChange}
+            />
+          </FormGroup>
+        </Form>
+      </Modal>
+    );
+  }
+}
+
+export default VMSnapshot;
