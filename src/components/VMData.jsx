2fb9fc1e920870253748580c175a4fded2fa419a
Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>
2024-06-05 09:54:07 +0200

    VMData: fetch VM information
    Fetch the state, the default node and the current node for all the VMs
    on the cluster.
    
    Signed-off-by: Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>

--- src/components/VMData.jsx
+++ src/components/VMData.jsx
@@ -17,6 +17,7 @@ class VMData extends React.Component {
 
   refreshVMList = async () => {
     await this.fetchVmInformations();
+    await this.fetchVmDefaultLocation();
   };
 
 
@@ -33,9 +34,71 @@ class VMData extends React.Component {
         currentNode: "-"
       }));
 
+      const statusPromises = VMlist.map((vm, index) =>
+        cockpit.spawn(["vm-mgr", "status", "--name", vm.name], { superuser: "try" })
+          .then(state => ({ index, state }))
+          .catch(error => {
+            console.error(`Error retrieving VM state ${vm.name}:`, error);
+            return { index, state: "Error" };
+          })
+      );
+
+      const locationPromises = VMlist.map((vm, index) =>
+        cockpit.spawn(["crm", "resource", "locate", vm.name], { superuser: "try" })
+          .then(output => {
+            const regex = /:(.*)/g;
+            let match = regex.exec(output);
+            const currentNode = match !== null ? match[1] : "-";
+            return { index, currentNode };
+          })
+          .catch(error => {
+            console.error(`Error retrieving VM state ${vm.name}:`, error);
+            return { index, currentNode: "Error" };
+          })
+      );
+
+      // Use Promises.all instead of a loop with "await cockpit.spawn()" to execute operations in parallel
+      const stateResults = await Promise.all(statusPromises);
+      const locationResults = await Promise.all(locationPromises);
+
+      stateResults.forEach(({ index, state }) => {
+        VMlist[index].state = state;
+      });
+
+      locationResults.forEach(({ index, currentNode }) => {
+        VMlist[index].currentNode = currentNode;
+      });
+
       this.setState({ VMlist });
     } catch (error) {
       console.error("Error retrieving the list of VMs:", error);
+    }
+  };
+
+  fetchVmDefaultLocation = async () => {
+    try {
+      const output = await cockpit.spawn(["crm", "configure", "show"], { superuser: "try"});
+      // Look for the occurences of "prefer" or "pin" to get informations about the collocation constraint of the deployed VMs
+      // (VM name + collocation type and associated node)
+      const regex = /(prefer|pin)-(?:\S+)\s(\S+)\s(?:\S+)\sinf:\s(.+)/g;
+      let match;
+      const VMlist = [...this.state.VMlist];
+
+      while ((match = regex.exec(output)) !== null) {
+        const resourceName = match[2];
+        const locationType = match[1];
+        const node = match[3];
+        const resourceIndex = VMlist.findIndex((vm) => vm.name === resourceName);
+        if (resourceIndex !== -1) {
+          VMlist[resourceIndex] = {
+            ...VMlist[resourceIndex],
+            defaultNode: locationType + ": " + node,
+          };
+        }
+      }
+      this.setState({ VMlist });
+    } catch (error) {
+      console.error("Error retrieving default VM locations:", error);
     }
   };
 
