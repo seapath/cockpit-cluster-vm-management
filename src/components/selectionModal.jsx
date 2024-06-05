01de9e9684aecaf002ce333d4e40e49fec31c48d
Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>
2024-06-05 14:07:35 +0200

    selectionModal: add React component
    This component opens a small window on the cockpit plugin page to select
    an option among the string array given in parameter. The selected string
    will be used by the function given to the parameter onConfirm.
    
    Signed-off-by: Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>

---
+++ src/components/selectionModal.jsx
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2024 Savoir-faire Linux Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import { Modal, ModalVariant, Button, Dropdown, MenuToggle, DropdownItem, DropdownList } from '@patternfly/react-core';
+
+class SelectionModal extends React.Component {
+  state = {
+    selectedItem: '',
+    isDropdownOpen: false,
+  };
+
+  handleDropdownToggle = () => {
+    this.setState({ isDropdownOpen: !this.state.isDropdownOpen });
+  };
+
+  handleDropdownSelect = (selectedItem) => {
+    this.setState({ selectedItem, isDropdownOpen: false });
+  };
+
+  handleConfirm = () => {
+    const { selectedItem } = this.state;
+    this.props.onConfirm(selectedItem);
+  };
+
+  handleCancel = () => {
+    this.setState({ selectedItem: '', isDropdownOpen: false });
+    this.props.onCancel();
+  };
+
+  render() {
+    const { isOpen, options, title } = this.props;
+    const { selectedItem, isDropdownOpen } = this.state;
+
+    return (
+      <Modal
+        title={title}
+        variant={ModalVariant.small}
+        isOpen={isOpen}
+        onClose={this.handleCancel}
+        actions={[
+          <Button key="confirm" variant="primary" onClick={this.handleConfirm}>
+            Confirm
+          </Button>,
+          <Button key="cancel" variant="link" onClick={this.handleCancel}>
+            Cancel
+          </Button>
+        ]}
+      >
+        <Dropdown
+          isOpen={isDropdownOpen}
+          onSelect={this.handleDropdownSelect}
+          onOpenChange={isOpen => this.setState({ isDropdownOpen: isOpen })}
+          toggle={toggleRef => (
+            <MenuToggle ref={toggleRef} onClick={this.handleDropdownToggle} isExpanded={isOpen}>
+              {selectedItem || "Select an option"}
+            </MenuToggle>
+          )}
+          ouiaId="BasicDropdown"
+          shouldFocusToggleOnSelect
+        >
+          <DropdownList>
+            {options.map((option, index) => (
+              <DropdownItem key={index} onClick={() => this.handleDropdownSelect(option)}>
+                {option}
+              </DropdownItem>
+            ))}
+          </DropdownList>
+        </Dropdown>
+      </Modal>
+    );
+  }
+}
+
+export default SelectionModal;
